572d4aec4bbbaea097342933864ffd1bd5d4abdf
Brian Weikel <brianweikel@msn.com>
2018-08-07 19:13:30 -0600

    updated javascript

--- assignment3/assets/javascript/game.js
+++ assignment3/assets/javascript/game.js
@@ -1,42 +1,39 @@
+// document.addEventListener('keydown', function(event) { });
 
 
-
-
-   /*  /------- GLOBAL VARIABLES ---------/   */ 
+/*  /------- GLOBAL VARIABLES ---------/   */
 
 
 var guessInput = document.getElementById("guessInput").value;
-var guessField = document.getElementById("guesses");
+var wrongGuesses = document.getElementById("guesses");
 var numOfGuesses = document.getElementById("guesses-left");
 var wins = document.getElementById("wins");
 var losses = document.getElementById("losses");
 var numOfGuesses = 10;
-var wins = 0;
-var losses = 0;
-
-
 
 
 
 /*  /------- FIRST GUESS/GENERATE RANDOM LETTER FUNCTION ---------/   */
 
-document.addEventListener('keydown', function(event) {
+$(document).ready(function() {
+  var letters =
+    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    var letters =
-        ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
-        ,"r","s","t","u","v","w","x","y","z"];
-    
-    var letter = letters[Math.floor(Math.random()*letters.length)];
-    console.log(letter);
+  var letter = letters[Math.floor(Math.random() * letters.length)];
+  console.log(letter);
 
-    var guessInput = document.getElementById("guessInput").value;
-  
-  
-  updateDisplayGuess(guessInput);
-  
+  wins.innerHTML = 0;
+  losses.innerHTML = 0;
 });
 
 
+
+document.addEventListener('keydown', function() {
+
+  var guessInput = document.getElementById("guessInput").value;
+
+  updateDisplayGuess(guessInput);
+});
 
 
 
@@ -44,55 +41,51 @@ document.addEventListener('keydown', function(event) {
 
 function updateDisplayGuess(guessInput) {
   var letters =
-        ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
-        ,"r","s","t","u","v","w","x","y","z"];
-    
-  var letter = letters[Math.floor(Math.random()*letters.length)];
+    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  var letter = letters[Math.floor(Math.random() * letters.length)];
   guessInput.value = "";
-  numOfGuesses--;
+  numOfGuesses.innerHTML--;
+
 
   if (numOfGuesses === 0 && guessInput) {
-    losses--;
     gameOver();
-  } else if (guessInput[i] > letter[i]) {
-    document.getElementById("lowOrHi").innerHTML = "That is too high. Try again.";
-  } else if (guessInput < letter[i]) {
-    document.getElementById("lowOrHi").innerHTML = "That is too low. Try again.";
-  } else (guessInput === letter[i]) 
-    document.getElementById("lowOrHi").innerHTML = "BOOM!";
-    wins++;
-    gameOver();
-  } 
+    newGame();
+  } else if (guessInput !== letter) {
+    wrongGuessList();
+  } else(guessInput === letter)
+  newGame();
+}
 
 
 
+/*  /------- GAME OVER/RESTART ---------/   */
 
+function gameOver() {
 
-/*  /------- GAME OVER ---------/   */
+  var letters =
+    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  function gameOver() {
-    guessField.innerHTML = "";
-  }
+  guessInput.innerHTML = "";
+  losses.innerHTML++;
+  numOfGuesses.innerHTML = 10;
+  wrongGuesses.innerHTML = "";
 
-
-
-
-
-
-
-
-
-
+  var letter = letters[Math.floor(Math.random() * letters.length)];
+}
 
 
 
+function newGame() {
+  guessInput.innerHTML = "";
+  wins.innerHTML++;
+  numOfGuesses.innerHTML = 10;
+  wrongGuesses.innerHTML = "";
+}
 
 
 
-
-
-  
-
-  
-
-  
+function wrongGuessList() {
+  // add the letter to wrong guesses
+  $("#guesses").append(guessInput + ", ");
+}
